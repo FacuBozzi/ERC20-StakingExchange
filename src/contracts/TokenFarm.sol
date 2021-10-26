d69bf0223dca761e30eec217039f7d77f1b6c951
facundo bozzi <facundo.bo@hotmail.com>
2021-10-26 18:07:52 -0300

    TokenFarm.sol done

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -5,8 +5,10 @@ import "./DaiToken.sol";
 
 contract TokenFarm {
     string public name = "Dapp Token Farm"; 
+    address public owner;
     DappToken public dappToken;
     DaiToken public daiToken;
+    
 
     address[] public stakers;
     mapping(address => uint) public stakingBalance;
@@ -16,9 +18,13 @@ contract TokenFarm {
     constructor(DappToken _dappToken, DaiToken _daiToken) public{
         dappToken = _dappToken;
         daiToken = _daiToken;
+        owner = msg.sender;
     }
 
-    function stateTokens(uint _amount) public {
+    function stakeTokens(uint _amount) public {
+
+        require(_amount > 0, "amount must be greater than 0");
+
         daiToken.transferFrom(msg.sender, address(this), _amount);
         //update staking balance
         stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
@@ -29,4 +35,22 @@ contract TokenFarm {
         hasStaked[msg.sender] = true;
         isStaking[msg.sender] = true;
     }
+
+    function issueToken() public{
+        require(msg.sender == owner, "only the owner can call this function");
+        for(uint i = 0; i < stakers.length; i++){
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient];
+            if(balance > 0) {
+                dappToken.transfer(recipient, balance);
+                }
+            }
+        }
+
+//     function withdrawTokens() public{
+//         for(uint i = 0; i < stakers.length; i++){
+//             if(isStaking[stakers[i]]){
+//                 dappToken.transferFrom(address(this), stakers[i], stakingBalance[stakers[i]]);
+//                 isStaking[stakers[i]] = false;
+//     }
 }
